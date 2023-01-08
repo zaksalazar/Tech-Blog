84a1d9417596b9c6d16455369158ea7f051f6a3a
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2023-01-07 20:49:13 -0800

    added routing logic and views

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -11,14 +11,38 @@ const withAuth = require("../utils/auth");
 // It should display all of the posts created by the logged in user
 router.get("/", withAuth, async (req, res) => {
   // TODO - retrieve all posts from the database for the logged in user
+  // const postData = await Post.findAll({
+  //   where: {
+  //     userId:req.session.userId
+  //   }
+  // })
+  // const posts = postData.map(post => post.get({plain:true}))
+  const posts = [
+    {
+      title: 'Hello World',
+    },
+    {
+      title: 'Hello World',
+    },
+    {
+      title: 'Hello World',
+    },
+    {
+      title: 'Hello World',
+    },
+  ]
+
   // render the dashboard template with the posts retrieved from the database
   //default layout is set to main.handlebars, layout need to be changed to dashboard to use dashboard.handlebars
-  res.render("admin-all-posts", { layout: "dashboard" });
+  res.render("admin-all-posts", { layout: "dashboard", posts });
   // refer to admin-all-posts.handlebars write the code to display the posts
 });
 
 // TODO - create logic for the GET route for /new that renders the new post page
 // It should display a form for creating a new post
+router.get("/new", withAuth, async(req,res) =>{
+  res.render('newpost', {layout: 'dashboard'}); 
+})
 
 // TODO - create logic for the GET route for /edit/:id that renders the edit post page
 // It should display a form for editing an existing post
