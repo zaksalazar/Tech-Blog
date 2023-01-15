424cb1e2466d069bf1aefc20f1026a0829f1aa0a
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2023-01-14 21:13:44 -0800

    updated post routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -6,10 +6,12 @@ const withAuth = require('../../utils/auth');
 // TODO - create a POST route for creating a new post
 // This should be a protected route, so you'll need to use the withAuth middleware
 router.post('/', withAuth, async (req, res) => {
+  const body = req.body
   try {
-    const post = await Post.create(req.body); 
-    res.status(201).json(post);
-  }
+    const post = await Post.create({ body, userid: req.session.userid }); 
+    console.log ('My post' , post)
+    res.json(post); 
+  } 
   catch (err) {
     res.status(400).json(err); 
   }
@@ -20,14 +22,15 @@ router.post('/', withAuth, async (req, res) => {
 // This should be a protected route, so you'll need to use the withAuth middleware
 router.put('/:id', withAuth, async (req, res) => {
   try {
-    const updatePost = await POST.update(req.body, {
+    const updatePost = await Post.update({ body: req.body,}
+      {
       where: {
         id: req.params.id
       }
     });
     if(!updatePost) {
       res.status(404).json({
-        message: 'No post found with this id' });
+        message: 'No post found with this id'  + req.params.id});
         return; 
       }
     res.status(200).json(updatePost); 
@@ -43,19 +46,19 @@ router.put('/:id', withAuth, async (req, res) => {
 router.delete('/:id', withAuth, async (req, res) => {
   // delete a category by its `id` value
   try {
-  const post = await Post.destroy ({
+  const deletePost = await Post.destroy ({
     where: {
     id: req.params.id
   }
   });
 
-    if (!post) {
+    if (!deletePost) {
       res.status(404).json({
         message: 'No post found with this id' });
         return;
     }
 
-    res.status(200).json(post);
+    res.status(200).json(deletePost);
   } catch (err) {
     res.status(500).json(err);
   }
