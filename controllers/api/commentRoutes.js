42bf47de47050f21901e767c9ce94fc762e3b240
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2023-01-14 21:03:52 -0800

    updated comment api routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,19 +1,33 @@
 
 const router = require('express').Router();
-const { Comment } = require('../../models/');
+const { Comment, User } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
 // TODO - create a POST route for creating a new comment
 // This should be a protected route, so you'll need to use the withAuth middleware
 router.post('/', withAuth, async (req, res) => {
+  const body = req.body
   try{
-    const comment = await Comment.create(req.body);
-    res.status(200).json(comment);
+    const comment = await Comment.create({...body, userId: req.user.id,});
+    res.json(addComment); 
   }
   catch (err) {
     res.status(400).json(err);
   }
 }); 
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+      const commentData = await Comment.findAll({
+          include : [User],
+      });
+      const postComments = commentData.map((comment) => comment.get({ plain: true }));
+      console.log(postComments)
+      res.render('single-post', { postComments, loggedIn: req.session.loggedIn});
+  } catch (err) {
+      res.status(400).json(err);
+      }
+  });
 
 module.exports = router;
 
