1c2159b959667cc555d14fb5949974db151c16e3
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2023-01-14 20:58:57 -0800

    updated home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,52 +1,51 @@
-
 // homeroutes contains all the view routes that do not require any authentication
 const router = require("express").Router();
 const { Post, Comment, User } = require("../models/");
 
 // TODO - work on GET route for getting all posts
 // this page can be viewed without logging in
+// TODO - retrieve all posts from the database
+// render the homepage template with the posts retrieved from the database
+// refer to homepage.handlebars write the code to display the posts
 router.get("/", async (req, res) => {
-  try { 
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['user_name'],
-        },
-      ],
-    });
-    res.render("homepage");
+  let username;
+  if (req.session.loggedIn) {
+    username = req.session.username;
   }
-  catch (err) {
+  try {
+    const postData = await Post.findAll({
+      include: [user],
+    });
+    res.render("homepage", {
+      posts,
+    });
+  } catch (err) {
     res.status(400).json(err);
   }
-  // TODO - retrieve all posts from the database
-  // render the homepage template with the posts retrieved from the database
-  // refer to homepage.handlebars write the code to display the posts
 });
 
 // TODO - create a GET route for getting a single post with its id
 // this page can be viewed without logging in
-router.get("/:id",  async (req, res) => {
+router.get("/post/:id", async (req, res) => {
   try {
-    const postData = await Post.findOne({
-      where: {
-        id: req.params.id,
-      },
+    const postData = await Post.findByPk(req.params.id, {
+      attributes: ["title", "body"],
       include: [
+        User,
         {
-          model: User,
-          attributes: ['user_name'],
+          model: Comment,
+          include: [User],
         },
       ],
     });
-  }
-  catch (err) {
-    res.status(400).json(err);
+    const post = postData.get({ plain: true });
+    res.render("post", {
+      ...post,
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
-
-
 
 // This route renders the login page, which has been completed for you
 router.get("/login", (req, res) => {
@@ -58,8 +57,6 @@ router.get("/login", (req, res) => {
   //render the login view otherwise, refer to login.handlebars
   res.render("login");
 });
-
-
 
 // This route renders the signup page, which has been completed for you
 router.get("/signup", (req, res) => {
@@ -73,4 +70,3 @@ router.get("/signup", (req, res) => {
 });
 
 module.exports = router;
-
